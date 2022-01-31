6dfd90d68ea76d34500389e1eb96dfa83ac1771e
prakashsellathurai <prakashsellathurai@gmail.com>
2022-01-31 15:25:32 +0530

    updated projects

---
+++ scripts/downloadGHData.py
@@ -0,0 +1,25 @@
+
+import urllib.request, json 
+from rich import print
+import yaml
+# with urllib.request.urlopen("https://api.github.com/users/prakashsellathurai/repos") as url:
+#     data = json.loads(url.read().decode())
+
+#     with open('./scripts/repos.json', 'w', encoding='utf-8') as f:
+#         json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+with open('./scripts/repos.json', 'r') as f:
+    data: list = json.load(f)
+data.sort(key= lambda datum: datum['pushed_at'],reverse=True)
+if data:
+    yamldata = []
+    for datum in data:
+        yamldata.append({
+            'name': datum['name'],
+            'url': datum['html_url'],
+            'desc': datum['description']
+        })
+
+    with open('_data/repos.yml', 'w') as yaml_file:
+        yaml.dump(yamldata, yaml_file, default_flow_style=False)

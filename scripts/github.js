0d4b1508579271f756098aa3cfb722e7c92e3685
Prakash Sellathurai <prakashsellathurai@gmail.com>
2025-02-17 04:43:21 +0530

    fix: bug in GitHub API fetch to handle pagination and retrieve all repositories

--- scripts/github.js
+++ scripts/github.js
@@ -1,12 +1,39 @@
 import fetch from 'node-fetch'
 import fs from 'fs'
 
-const GITHUB_API_URL = 'https://api.github.com/users/prakashsellathurai/repos'
+const GITHUB_API_URL = 'https://api.github.com/users/prakashsellathurai/repos?per_page=100'
+
+async function fetchAllRepos(url) {
+  let repos = []
+  let nextUrl = url
+
+  while (nextUrl) {
+    const response = await fetch(nextUrl)
+    const data = await response.json()
+    repos = repos.concat(data)
+
+    const linkHeader = response.headers.get('link')
+    if (linkHeader) {
+      const links = linkHeader.split(',').reduce((acc, link) => {
+        const match = link.match(/<([^>]+)>;\s*rel="([^"]+)"/)
+        if (match) {
+          acc[match[2]] = match[1]
+        }
+        return acc
+      }, {})
+
+      nextUrl = links.next
+    } else {
+      nextUrl = null
+    }
+  }
+
+  return repos
+}
 
 async function fetchRepos() {
   try {
-    const response = await fetch(GITHUB_API_URL)
-    const data = await response.json()
+    const data = await fetchAllRepos(GITHUB_API_URL)
 
     // Sort data by 'pushed_at' in descending order
     data.sort((a, b) => new Date(b.pushed_at) - new Date(a.pushed_at))

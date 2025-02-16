2bf42310b15188e951b9bcb1f8359467b975bae9
Prakash Sellathurai <prakashsellathurai@gmail.com>
2025-02-17 04:36:21 +0530

    feat: update sampleProjects to preserve order of showcase projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -6,7 +6,18 @@ interface Project {
   imgSrc?: string
 }
 
+const projectsNamesToShowcase = [
+  'keras',
+  'pyqoi',
+  'DeFIR',
+  'CLothXMicroServices',
+  'openpose-docker',
+  'Heimdall',
+]
+
 const projectsData: Project[] = repos as Project[]
-const sampleProjects: Project[] = repos.slice(0, 6) as Project[]
+const sampleProjects: Project[] = projectsNamesToShowcase // preserve order of projectsNamesToShowcase
+  .map((name) => projectsData.find((project) => project.title === name))
+  .filter((project): project is Project => project !== undefined)
 
 export { projectsData, sampleProjects }

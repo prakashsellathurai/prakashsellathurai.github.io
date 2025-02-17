8114a26032adb2fa21c48234835502e66fa8b90c
Prakash Sellathurai <prakashsellathurai@gmail.com>
2025-02-17 17:30:59 +0530

    feat: update sitemap routes to include new sections and correct URL formatting

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -14,8 +14,8 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: post.lastmod || post.date,
     }))
 
-  const routes = ['', 'blog', 'projects', 'tags'].map((route) => ({
-    url: `${siteUrl}/${route}`,
+  const routes = ['', 'essays', 'projects', 'tags', 'about', 'bookmarks'].map((route) => ({
+    url: `${siteUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
 

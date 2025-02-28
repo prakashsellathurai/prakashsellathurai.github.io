4d4186e50e5861e1ce45eb7e530904f673b68c76
Prakash Sellathurai <prakashsellathurai@gmail.com>
2025-02-28 16:11:08 +0530

    fix: update terminology from "Article" to "Essay" in PostLayout

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -116,7 +116,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                       {prev && prev.path && (
                         <div>
                           <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
-                            Previous Article
+                            Previous Essay
                           </h2>
                           <div className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
                             <Link href={`/${prev.path}`}>{prev.title}</Link>
@@ -126,7 +126,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                       {next && next.path && (
                         <div>
                           <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
-                            Next Article
+                            Next Essay
                           </h2>
                           <div className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
                             <Link href={`/${next.path}`}>{next.title}</Link>
@@ -140,9 +140,9 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                   <Link
                     href={`/${basePath}`}
                     className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                    aria-label="Back to the blog"
+                    aria-label="Back to the Essays"
                   >
-                    &larr; Back to the blog
+                    &larr; Back to the Essays
                   </Link>
                 </div>
               </footer>

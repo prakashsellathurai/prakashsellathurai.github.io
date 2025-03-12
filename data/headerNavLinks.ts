cf384a753101d7e866ad7100aff53a7e6ba776fa
Prakash Sellathurai <prakashsellathurai@gmail.com>
2025-03-12 13:34:24 +0530

    add: include link to Prakash's resume in header navigation

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,5 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
+  { href: '/static/resume/prakash_s_resume.pdf', title: 'Resume' },
   { href: '/essays', title: 'Essays' },
   { href: '/projects', title: 'Projects' },
   { href: '/tags', title: 'Tags' },

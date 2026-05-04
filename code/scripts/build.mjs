c852cbaf68bc514e733c4e9534f0c2f1155e2f9a
Prakash Sellathurai <prakashsellathurai@gmail.com>
2026-05-04 18:18:00 +0530

    fix favicon

--- code/scripts/build.mjs
+++ code/scripts/build.mjs
@@ -169,12 +169,19 @@ class MarkdownRenderer {
 class TemplateRenderer {
   static renderHead(metadata, { title, description }) {
     const cssLink = '<link rel="stylesheet" href="/static/css/style.css">';
+    const favicon = `
+  <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png">
+  <link rel="manifest" href="/static/favicons/site.webmanifest">
+  <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#8b7355">`;
     return `
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>${Formatter.escapeHtml(title)}</title>
   <meta name="description" content="${Formatter.escapeHtml(description)}">
+  ${favicon}
   ${cssLink}
 </head>
 `;

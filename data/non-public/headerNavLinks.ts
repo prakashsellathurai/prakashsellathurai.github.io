87387590e2ddd90a957fa0609a1b5ed86f9d71c0
Prakash Sellathurai <prakashsellathurai@gmail.com>
2026-05-15 15:26:46 +0530

    remove legacy

--- data/non-public/headerNavLinks.ts
+++
@@ -1,9 +0,0 @@
-const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/static/resume/prakash_s_resume.pdf', title: 'Resume' },
-  { href: '/essays', title: 'Essays' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/bookshelf', title: 'Bookshelf' },
-]
-
-export default headerNavLinks
